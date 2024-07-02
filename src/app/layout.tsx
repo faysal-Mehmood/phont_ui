a369bf4025ecfb8c35c267885245c448b4cb3102
faysal-Mehmood <faysal.mehmood101@gmail.com>
2024-07-03 01:06:17 +0500

    inital update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme";
+import "../assets/styles/globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        {" "}
+        <AppRouterCacheProvider>
+          + <ThemeProvider theme={theme}>{children}+ </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
