9c23a7947edc802d62022043bd131e3399409b00
Muhammad Qamar <muhammad@3lanemarketing.com>
2024-07-10 00:35:44 +0500

    added icons

--- src/components/sideBar/index.tsx
+++ src/components/sideBar/index.tsx
@@ -1,85 +1,44 @@
 "use client";
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Avatar, Box, Typography } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 
-import SideBarData from "../../../public/data/sidebar.json";
+import { SideBarData } from "@/dats/sidebar";
+
+import styles from "./sidebar.module.scss";
 
 const SideBar = () => {
   return (
-    <Box
-      height={"calc(100vh - 90px)"}
-      bgcolor='#000000'
-      position={"fixed"}
-      top={0}
-      left={0}
-      padding={"40px 75px 52px 34px"}
-      zIndex={5}>
-      <Box
-        width={"100%"}
-        height={"100%"}
-        display={"flex"}
-        flexDirection={"column"}
-        justifyContent={"space-between"}
-        alignItems={"flex-start"}
-        gap={"24px"}>
-        <Image
-          src={SideBarData?.webLogo}
-          alt={"logo"}
-          width={101}
-          height={22}
-        />
+    <Box className={styles.SidebarMainBox}>
+      <Box className={styles.SidebarBox}>
+        <Box>
+          <Image
+            src={SideBarData?.webLogo}
+            alt={"logo"}
+            width={101}
+            height={22}
+            className={styles.WebLogo}
+          />
 
-        <Box
-          width={"100%"}
-          display={"flex"}
-          flexDirection={"column"}
-          alignItems={"flex-start"}
-          gap={"16px"}>
-          {SideBarData?.links?.map((item, index) => (
-            <Box
-              key={index}
-              width={"100%"}
-              display={"flex"}
-              alignItems={"center"}
-              justifyContent={"flex-start"}
-              gap={"16px"}
-              padding={"9px 20px"}
-              borderRadius={"24px"}
-              sx={{
-                "&:hover": {
-                  backgroundColor: "#D2BEFF",
-                  cursor: "pointer",
-                  h6: {
-                    color: "#000000",
-                  },
-                },
-                "&:active": {
-                  backgroundColor: "#444444",
-                },
-              }}>
-              <Image src={item?.icon} alt={"logo"} width={20} height={16} />
-              <Typography variant='h6' fontWeight={400} color={"#fff"}>
-                {item.name}
-              </Typography>
-            </Box>
-          ))}
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            gap: "16px",
-          }}>
-          <Box
-            sx={{
-              position: "relative",
-              width: "55px",
-              height: "55px",
-              borderRadius: "50%",
-            }}>
-            <Image src={"/public/images/xd.jpg"} alt={"logo"} fill />
+          <Box className={styles.SideBarMenu}>
+            {SideBarData?.links?.map((item, index) => (
+              <Box key={index} className={styles.MenuItem}>
+                {item.icon}
+                <Typography variant='h6' fontWeight={400} color={"#fff"}>
+                  {item.name}
+                </Typography>
+              </Box>
+            ))}
           </Box>
+        </Box>
+        <Box className={styles.SideBarUserAbout}>
+          <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
+          {SideBarData?.userAbout?.map((item, index) => (
+            <Link key={index} href={item.url}>
+              {item.icon}
+            </Link>
+          ))}
         </Box>
       </Box>
     </Box>
---
+++ src/dats/sidebar.tsx
@@ -0,0 +1,83 @@
+import SubtitlesOutlinedIcon from "@mui/icons-material/SubtitlesOutlined";
+import FormatShapesOutlinedIcon from "@mui/icons-material/FormatShapesOutlined";
+import TranslateOutlinedIcon from "@mui/icons-material/TranslateOutlined";
+import BadgeOutlinedIcon from "@mui/icons-material/BadgeOutlined";
+import PublicOutlinedIcon from "@mui/icons-material/PublicOutlined";
+import MoodOutlinedIcon from "@mui/icons-material/MoodOutlined";
+import GraphicEqOutlinedIcon from "@mui/icons-material/GraphicEqOutlined";
+import MusicNoteRoundedIcon from "@mui/icons-material/MusicNoteRounded";
+import RecordVoiceOverOutlinedIcon from "@mui/icons-material/RecordVoiceOverOutlined";
+import HearingDisabledOutlinedIcon from "@mui/icons-material/HearingDisabledOutlined";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+
+export const SideBarData = {
+  webLogo: "/images/Phont_Logo_Weiss.svg",
+  links: [
+    {
+      name: "Basics",
+      url: "/",
+      icon: <SubtitlesOutlinedIcon />,
+    },
+    {
+      name: "Transcription",
+      url: "/",
+      icon: <FormatShapesOutlinedIcon />,
+    },
+    {
+      name: "Translation",
+      url: "/",
+      icon: <TranslateOutlinedIcon />,
+    },
+    {
+      name: "Speaker",
+      url: "/",
+      icon: <BadgeOutlinedIcon />,
+    },
+    {
+      name: "Phonetics",
+      url: "/",
+      icon: <PublicOutlinedIcon />,
+    },
+    {
+      name: "Emotion",
+      url: "/",
+      icon: <MoodOutlinedIcon />,
+    },
+    {
+      name: "Noise",
+      url: "/",
+      icon: <GraphicEqOutlinedIcon />,
+    },
+    {
+      name: "Music",
+      url: "/",
+      icon: <MusicNoteRoundedIcon />,
+    },
+    {
+      name: "Video",
+      url: "/",
+      icon: <RecordVoiceOverOutlinedIcon />,
+    },
+    {
+      name: "Accessibility",
+      url: "/",
+      icon: <HearingDisabledOutlinedIcon />,
+    },
+  ],
+  userAbout: [
+    {
+      url: "/",
+      icon: <ShoppingBagOutlinedIcon />,
+    },
+    {
+      url: "/",
+      icon: <HelpOutlineOutlinedIcon />,
+    },
+    {
+      url: "/",
+      icon: <SettingsOutlinedIcon />,
+    },
+  ],
+};
