a186f3b8d8f47deecf344af880dddedf9ead7fc6
Muhammad Qamar <muhammad@3lanemarketing.com>
2024-07-10 22:08:11 +0500

    fix conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,28 +9,23 @@ export default function Home() {
   const [activetab, setActiveTab] = useState("login");
   return (
     <main>
-      <div className="auth-wrapper">
-        <div className="auth-left-image">
+      <div className='auth-wrapper'>
+        <div className='auth-left-image'>
           <Image
-            src="/images/UX_Picture.jpg"
-            alt="left iamge"
-<<<<<<< HEAD
-            width={1080}
-            height={600}
-=======
+            src='/images/UX_Picture.jpg'
+            alt='left iamge'
             width={730}
             height={598}
->>>>>>> 87530edf38eda3aec35cc2fe805c39b6c1a8346d
           />
         </div>
-        <div className="form-wrapper">
-          <div className="auth-tabs">
-            <div className="button-tab" onClick={() => setActiveTab("login")}>
+        <div className='form-wrapper'>
+          <div className='auth-tabs'>
+            <div className='button-tab' onClick={() => setActiveTab("login")}>
               <h3 className={`${activetab === "login" ? "active-tab" : ""}`}>
                 Login
               </h3>
             </div>
-            <div className="button-tab" onClick={() => setActiveTab("signup")}>
+            <div className='button-tab' onClick={() => setActiveTab("signup")}>
               <h3 className={`${activetab === "signup" ? "active-tab" : ""}`}>
                 Register
               </h3>
